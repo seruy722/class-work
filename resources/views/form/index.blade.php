a2a9dd3f63125ac6a222ca8dfbd19b3e3d2b4155
Группа №1 <pleomax3002@yahoo.com>
2018-04-06 18:36:30 +0300

    soap curl

--- resources/views/form/index.blade.php
+++ resources/views/form/index.blade.php
@@ -2,9 +2,9 @@
 
 
 @section('content')
- <p><a href="{{ route('form.create') }}">Создать</a></p>
+ <!--<p><a href="{{ route('form.create') }}">Создать</a></p>-->
  <table class="table-striped  table">
-    @foreach($users as $user)
+<!--    @foreach($users as $user)
     <tr>
         <td>{{ $user->id}}</td>
         <td>{{ $user->name}}</td>
@@ -12,11 +12,37 @@
         <td><a href="{{ route('form.update',$user->id) }}">Редактировать</a></td>
         <td><a href="{{ route('form.delete',$user->id) }}">Удалить</a></td>
     </tr>
-    @endforeach
+    @endforeach-->
     
 </table>
- {{$users->links()}}
+<!-- {{$users->links()}}
      @foreach($sub as $user)
         {{$user}}
     @endforeach
-@endsection
+@endsection-->
+@php 
+    $curl = curl_init('http://www.cbr.ru/rss/RssNews');
+        //запись идентична
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    //curl_setopt($curl,CURLOPT_NOBODY,1);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    $xml   = new SimpleXMLElement($result);;
+
+    
+var_dump($xml);
+
+$soap = new SoapClient('http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?wsdl');
+$res = $soap->GetCursOnDateXML(['On_date'=>'2018-05-06']);
+
+$str = $res->GetCursOnDateXMLResult->any;
+$xml   = simplexml_load_string($str);
+$array = json_decode(json_encode((array) $xml), true);
+$array = array($xml->getName() => $array);
+
+
+
+
+
+@endphp
+
